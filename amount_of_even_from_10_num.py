9b30fb4ee92b57a64c3f743b17725d4b1fdf8ce5
angelalucban07-bit <angelalucban07@gmail.com>
2026-03-12 23:56:01 +0800

    created a program that counts the even numbers from 10 numbers input from the user, edited some variable names because even though it works some red appears

--- amount_of_even_from_10_num.py
+++ amount_of_even_from_10_num.py
@@ -1,6 +1,6 @@
-even = 0
+even_num = 0
 for i in range(10):
     num = float(input("Enter the number: "))
     if num % 2 == 0:
-        even += 1
-print("The amount of even numbers are ", even)
+        even_num += 1
+print("The amount of even numbers are:   ", even_num)
